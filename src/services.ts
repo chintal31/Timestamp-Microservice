d6219b853d5d6a5b117046dbea9dbaa4c43673e8
Chintal Dholakiya <chintald@mail.uni-paderborn.de>
2021-10-29 10:42:31 +0200

    fixed path error

--- src/services.ts
+++ src/services.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 
 export function home(req: Request, res: Response) {
-  res.sendFile(__dirname + "/src/index.html");
+  res.sendFile(__dirname + "/index.html");
 }
 
 export function date(req: Request, res: Response) {
