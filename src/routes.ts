905c5cd59fe99ffda8c03ec1fa2b854f026e8881
Chintal Dholakiya <chintald@mail.uni-paderborn.de>
2021-10-29 10:27:37 +0200

    created timestamp apis

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import { Application } from "express";
-import { helloWorld } from "./services";
+import { helloWorld, date, currentTime } from "./services";
 
 export const routes = (app: Application) => {
   app.get("/", helloWorld);
+  app.get("/api/:date", date);
+  app.get("/api/", currentTime);
 };
--- src/services.ts
+++ src/services.ts
@@ -3,3 +3,31 @@ import { Request, Response } from "express";
 export function helloWorld(req: Request, res: Response) {
   res.status(200).json("Hello World!");
 }
+
+export function date(req: Request, res: Response) {
+  let givenDate: number | Date;
+  if (isUNIX(req.params.date)) {
+    givenDate = parseInt(req.params.date);
+    res
+      .status(200)
+      .json({ unix: givenDate, utc: new Date(givenDate).toUTCString() });
+  } else {
+    givenDate = new Date(req.params.date);
+    if (givenDate.toString() == "Invalid Date") {
+      return res.json({ error: "Invalid Date" });
+    }
+    res
+      .status(200)
+      .json({ unix: givenDate.getTime(), utc: givenDate.toUTCString() });
+  }
+}
+
+function isUNIX(date: string): boolean {
+  return /\d{5,}/.test(date);
+}
+
+export function currentTime(req: Request, res: Response) {
+  return res
+    .status(200)
+    .json({ unix: Date.now(), utc: new Date().toUTCString() });
+}
